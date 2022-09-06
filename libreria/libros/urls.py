d49701e6850d4a85c32e33790d82020fdfcf0fe8
Daniel Tavera <daniel2194144@correo.uis.edu.co>
2022-09-06 07:53:10 -0500

    update

---
+++ libreria/libros/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    #ruta, vista, nombre interno
+    path('',views.index, name='index'),
+    path('libros/api/', views.LibroListApiView.as_view()),
+    path('libros/api/<int:libro_id>/', views.LibroDetailApiView.as_view()),
+]
